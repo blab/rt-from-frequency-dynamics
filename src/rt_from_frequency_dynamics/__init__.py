5ed8ba2dbd31f540617062c780e5131d90c38446
marlinfiggins <marlinfiggins@gmail.com>
2022-01-08 11:02:01 -0800

    Changing exports

--- src/rt_from_frequency_dynamics/__init__.py
+++ src/rt_from_frequency_dynamics/__init__.py
@@ -1,10 +1,12 @@
 from .datahelpers import format_seq_names, counts_to_matrix
 from .datahelpers import prep_dates, prep_cases, prep_sequence_counts 
 
-from .modeldata import LineageData
+from .modeldata import CaseData, VariantData
 
-from .modelhelpers import continuous_dist_to_pmf, discretise_gamma, discretise_lognorm, is_obs_idx,pad_delays, get_standard_delays
-from .modelhelpers import make_breakpoint_splines, is_obs_idx, pad_to_obs
+from .modelhelpers import continuous_dist_to_pmf, discretise_gamma, discretise_lognorm, is_obs_idx, pad_delays, get_standard_delays
+from .modelhelpers import is_obs_idx, pad_to_obs
+
+from .Splines import Spline, SplineDeriv
 
 from .modelfunctions import get_infections, apply_delay, forward_simulate_I, reporting_to_vec 
 
@@ -14,18 +16,19 @@ from .modeloptions import GARW, FixedGA, FreeGrowth
 from .modeloptions import PoisCases, NegBinomCases
 from .modeloptions import MultinomialSeq, DirMultinomialSeq
 
-from .lineagemodel import LineageModel, FixedGrowthModel, FreeGrowthModel, GARandomWalkModel
+from .renewalmodel import RenewalModel, FixedGrowthModel, FreeGrowthModel, GARandomWalkModel
+from .exponentialmodel import ExpModel
 
 from .inferencehandler import SVIHandler, MCMCHandler
 
-from .posteriorhelpers import get_R, get_growth_advantage, get_growth_advantage_time, get_little_r, get_I, get_freq
+from .posteriorhelpers import get_R, get_growth_advantage, get_growth_advantage_time, get_little_r, get_little_r_gen, get_I, get_freq
 
 from .posteriorhandler import PosteriorHandler, MultiPosterior
 
 from .plotfunctions import *
 
-from .analysishelpers import get_location_LineageData, fit_SVI, fit_SVI_locations 
+from .analysishelpers import get_location_VariantData, fit_SVI, fit_SVI_locations 
 from .analysishelpers import save_posteriors, sample_loaded_posterior, unpack_model
-from .analysishelpers import gather_R, gather_little_r, gather_ga, gather_ga_time, gather_I, gather_freq
+from .analysishelpers import gather_R, gather_little_r, gather_little_r_gen, gather_ga, gather_ga_time, gather_I, gather_freq
 
 from .pipelinehelpers import make_path_if_absent, make_model_directories
