00d0c8b156a3bdb60e1a3f62ffdac43a9faf8025
marlinfiggins <marlinfiggins@gmail.com>
2022-03-15 15:11:18 -0700

    MLR updates

--- src/rt_from_frequency_dynamics/mlr.py
+++ src/rt_from_frequency_dynamics/mlr.py
@@ -5,6 +5,7 @@ import numpyro
 import numpyro.distributions as dist
 from .datahelpers import prep_dates, prep_sequence_counts
 
+
 class MLRData():
     def __init__(self, raw_seqs):
         self.dates, date_to_index = prep_dates(raw_seqs.date)
@@ -21,19 +22,25 @@ def MLR_numpyro(seq_counts, N, X, tau):
     _, N_features = X.shape
 
     # Sampling parameters
-    raw_beta = numpyro.sample("beta", 
-                   dist.Normal(0.0, 3.0),
-                   sample_shape = (N_features, N_variants-1))
-    beta = jnp.column_stack((raw_beta, jnp.zeros(N_features))) # Use zeros so that last column is pivot
+    raw_beta = numpyro.sample(
+        "raw_beta", 
+        dist.Normal(0.0, 3.0),
+        sample_shape = (N_features, N_variants-1)
+    )
+
+    beta = numpyro.deterministic(
+        "beta",
+        jnp.column_stack((raw_beta, jnp.zeros(N_features))) # Use zeros so that last column is pivot
+    )
 
     logits = jnp.dot(X, beta) # Logit frequencies by variant
 
     # Evaluate likelihood
-    numpyro.sample("obs", 
-                   dist.MultinomialLogits(
-                       logits= logits,
-                       total_count=jnp.nan_to_num(N)),
-                   obs=seq_counts)
+    numpyro.sample(
+        "obs", 
+        dist.MultinomialLogits(logits= logits, total_count=jnp.nan_to_num(N)),
+        obs=seq_counts
+    )
 
     # Compute frequency
     numpyro.deterministic("freq", softmax(logits, axis=-1))
@@ -49,9 +56,17 @@ class MultinomialLogisticRegression():
     
     def make_model(self):
         self.model = MLR_numpyro
+    
+    @staticmethod
+    def make_ols_feature(start, stop):
+        """
+        Given start and stop time, return feature matrix for MLR. 
+        One column is the bias term (all ones), the second is date as integer.
+        """
+        t = jnp.arange(start=start, stop=stop)
+        return jnp.column_stack((jnp.ones_like(t), t))
 
     def augment_data(self, data):
         T = len(data["N"])
         data["tau"] = self.tau
-        data["X"] = jnp.column_stack((jnp.ones(T), jnp.arange(T))) # Use intercept and time as predictors
-
+        data["X"] = self.make_ols_feature(0, T) # Use intercept and time as predictors
